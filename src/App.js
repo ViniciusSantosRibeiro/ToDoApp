35e1a5ff84fe2a606634166df994f8fe65cd749d
LitWaves Studios™ <vinicius.santos.ribeiro.v@gmail.com>
2024-09-29 15:09:35 -0300

    On branch master  Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.css
    	modified:   src/App.js
    	new file:   src/components/Task.js
    	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Icon from '@mdi/react';
+import { mdiDelete, mdiPlusThick} from '@mdi/js';
+import 'animate.css';
+
+import Task from './components/Task'
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  const [newTask, setNewTask] = useState('');
+
+  const [description, setTaskDescription] = useState('');
+
+  function addItem(event, index) {
+    if(newTask.length >= 3){
+      if(description.length <= 3){
+        alert('Criando tarefa sem descrição!');
+      }
+      setTasks([...tasks, {taskTitle: newTask, taskDescription: description || 'No description'}]);
+      setNewTask('');
+    }else{
+      alert('Coloque um título para sua tarefa!');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height: 'auto', width:'100%', margin: 'auto', color: 'white'}}>
+      <br></br>
+      <h1>To Do</h1>
+      <p>Organize, prioritize, and accomplish more!</p>
+
+      <br></br>
+
+      <div className='content' style={{display: 'flex', margin: 'auto', textAlign: 'center'}}>
+        <input className='inputTask' style={{height: '40px', width: '150px'}} type="text" placeholder="Task Title..." value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
+        <input className='inputTask' style={{height: '40px', width: '250px'}} type="text" placeholder="Task Description..." value={description} onChange={(e) => setTaskDescription(e.target.value)}/>
+        <button className='buttonTask' style={{margin: '0 5px', height: '40px', width: '40px', backgroundColor: 'white', border: 'none', borderRadius: '50px'}} onClick={addItem}><Icon style={{margin: '5px 0 0 0'}} path={mdiPlusThick} size={1.2}></Icon></button>
+      </div>
+
+      <br></br>
+
+      {tasks.map((task) => <Task taskTitle={task.taskTitle} taskDescription={task.taskDescription}></Task>)}
+
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import Icon from '@mdi/react';
+import { mdiDelete } from '@mdi/js';
+
+function Task({taskTitle, taskDescription}){
+    
+    const [completed, setCompleted] = useState(false)
+
+    const date = new Date();
+    const today = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const min = date.getMinutes();
+    const hours = date.getHours();
+
+		function deleteTask(index){
+			
+		}
+
+    function completedTask(){
+      setCompleted(!completed)
+    }
+
+		console.log(completed);
+    
+    return(
+        <div className='test' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+          <div style={{height: '100%', width: '350px', margin: '10px 0', textAlign: 'center', backgroundColor: 'white', color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '10px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)', transition: 'all 0.5s ease-in-out'}} onMouseEnter={(e) => {e.target.style.transform = 'scale(1.0)';}} onMouseLeave={(e) => {e.target.style.transform = 'scale(1)';}}>
+						 
+            <div style={{ justifyContent: 'center', marginBottom: '0.5em', backgroundColor: 'white' }}>
+              <h4 style={{ margin: 0 }}>{taskTitle}</h4>
+            </div>
+
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: '5px'}}>
+              <div className="task-content" style={{maxWidth: '240px', overflowWrap: 'break-word', backgroundColor: 'white'}}>
+                <p style={{ wordWrap: 'break-word', whiteSpace: 'normal'}}>{taskDescription}</p>
+              </div>
+
+              <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', gap: '5px', flexGrow: 2 }}>
+                <button className='button completed' onClick={() => completedTask}>Concluída</button>
+                <button className='button recicle' onClick={() => completedTask}><Icon path={mdiDelete} size={1}></Icon></button> 
+              </div>
+            </div>
+
+            <div>
+              <footer style={{fontSize: '12px', color: 'gray'}}>{today} / {month} / {year} - {hours}:{min}</footer>
+            </div>
+          </div>
+      </div>
+    )
+}
+
+export default Task
