25e12960a5fd5543f54498431149d096fe669f1e
LitWaves Studios™ <vinicius.santos.ribeiro.v@gmail.com>
2024-09-29 15:39:42 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { useState } from 'react'
 import Icon from '@mdi/react';
-import { mdiDelete, mdiPlusThick} from '@mdi/js';
+import {mdiPlusThick} from '@mdi/js';
 import 'animate.css';
 
 import Task from './components/Task'
@@ -16,9 +16,9 @@ function App() {
   function addItem(event, index) {
     if(newTask.length >= 3){
       if(description.length <= 3){
-        alert('Criando tarefa sem descrição!');
+        alert('Creating a task with no description!');
       }
-      setTasks([...tasks, {taskTitle: newTask, taskDescription: description || 'No description'}]);
+      setTasks([...tasks, {taskTitle: newTask, taskDescription: description || 'No descriptions'}]);
       setNewTask('');
     }else{
       alert('Coloque um título para sua tarefa!');
@@ -36,7 +36,7 @@ function App() {
       <div className='content' style={{display: 'flex', margin: 'auto', textAlign: 'center'}}>
         <input className='inputTask' style={{height: '40px', width: '150px'}} type="text" placeholder="Task Title..." value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
         <input className='inputTask' style={{height: '40px', width: '250px'}} type="text" placeholder="Task Description..." value={description} onChange={(e) => setTaskDescription(e.target.value)}/>
-        <button className='buttonTask' style={{margin: '0 5px', height: '40px', width: '40px', backgroundColor: 'white', border: 'none', borderRadius: '50px'}} onClick={addItem}><Icon style={{margin: '5px 0 0 0'}} path={mdiPlusThick} size={1.2}></Icon></button>
+        <button className='buttonTask' style={{margin: '0 5px', height: '40px', width: '40px', backgroundColor: 'white', border: 'none', borderRadius: '50px'}} onClick={addItem}><Icon style={{margin: ' 0 0 0'}} path={mdiPlusThick} size={1.2}></Icon></button>
       </div>
 
       <br></br>
