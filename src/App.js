fdcec3644465fd967f1da3d08aa692c134005b43
LitWaves Studios™ <vinicius.santos.ribeiro.v@gmail.com>
2024-09-30 18:46:15 -0300

    modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Task.js

--- src/App.js
+++ src/App.js
@@ -13,10 +13,18 @@ function App() {
 
   const [description, setTaskDescription] = useState('');
 
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString().split('T')[0];
+  const [deadlineDates, setDateTime] = useState([]);
+
   function addItem(event, index) {
     if(newTask.length >= 3){
-      setTasks([...tasks, {taskTitle: newTask, taskDescription: description || 'No description'}]);
+      setTasks([...tasks, {taskTitle: newTask, taskDescription: description || 'No description', taskDate: deadlineDates.length >= 1 || formattedDate}]);
+
+      setDateTime('');
       setNewTask('');
+      setTaskDescription('');
+      
     }else{
       alert('[ERROR] No task title!')
     }
@@ -36,12 +44,16 @@ function App() {
       <div className='content' style={{display: 'flex', margin: 'auto', textAlign: 'center'}}>
         <input className='inputTask' style={{height: '40px', width: '150px'}} type="text" placeholder="Task Title..." value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
         <input className='inputTask' style={{height: '40px', width: '250px'}} type="text" placeholder="Task Description..." value={description} onChange={(e) => setTaskDescription(e.target.value)}/>
+        <input className='inputTask' type='date' value={deadlineDates} onChange={(e) => setDateTime(e.target.value)}></input>
         <button type='submit' className='buttonTask' style={{margin: '0 5px', height: '40px', width: '40px', backgroundColor: 'white', border: 'none', borderRadius: '50px'}} onClick={addItem}><Icon style={{margin: '5px 0 0 0'}} path={mdiPlusThick} size={1.2}></Icon></button>
       </div>
 
       <br></br>
 
-      {tasks.map((task) => <Task taskTitle={task.taskTitle} taskDescription={task.taskDescription}></Task>)}
+      <div className='divTasks' style={{display: 'grid', justifyContent: 'center', columnGap: '20px' , rowGap: '20px', margin: '0 auto'}}>
+        {tasks.map((task) => <Task taskTitle={task.taskTitle} taskDescription={task.taskDescription} taskDate={task.taskDate}></Task>)}  
+      </div>
+      
 
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,30 +3,23 @@ import Icon from '@mdi/react';
 import { mdiDelete } from '@mdi/js';
 import 'animate.css';
 
-function Task({taskTitle, taskDescription}){
+function Task({taskTitle, taskDescription, taskDate}){
     
     const [completed, setCompleted] = useState(false)
 
-    const date = new Date();
-    const today = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    const min = date.getMinutes();
-    const hours = date.getHours();
-
 		function deleteTask(index){
+      console.log('Deleted task'); 
 			return
 		}
 
     function completedTask(){
-      setCompleted(!completed)
+      setCompleted(!completed);
+      console.log(completed);
     }
-
-		console.log(completed);
     
     return(
         <div className='animate__bounceIn' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-          <div style={{height: '100%', width: '350px', margin: '10px 0', textAlign: 'center', backgroundColor: 'white', color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '10px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)', transition: 'all 0.5s ease-in-out'}} onMouseEnter={(e) => {e.target.style.transform = 'scale(1.0)';}} onMouseLeave={(e) => {e.target.style.transform = 'scale(1)';}}>
+          <div style={{height: '100%', width: '350px', margin: '0px 0', textAlign: 'center', backgroundColor: 'white', color: 'black', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '10px', borderRadius: '5px', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.2)', transition: 'all 0.5s ease-in-out'}} onMouseEnter={(e) => {e.target.style.transform = 'scale(1.0)';}} onMouseLeave={(e) => {e.target.style.transform = 'scale(1)';}}>
 						 
             <div style={{ justifyContent: 'center', marginBottom: '0.5em', backgroundColor: 'white' }}>
               <h4 style={{ margin: 0 }}>{taskTitle}</h4>
@@ -38,13 +31,13 @@ function Task({taskTitle, taskDescription}){
               </div>
 
               <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end', gap: '5px', flexGrow: 2 }}>
-                <button className='button completed' onClick={() => completedTask}>Concluída</button>
-                <button className='button recicle' onClick={() => completedTask}><Icon path={mdiDelete} size={1}></Icon></button> 
+                <button className='button completed ' onClick={() => completedTask} onMouseEnter={(e) => {e.target.style.transform = 'scale(1.2)';}} onMouseLeave={(e) => {e.target.style.transform = 'scale(1)';}}>Concluída</button>
+                <button className='button recicle' onClick={() => deleteTask} onMouseEnter={(e) => {e.target.style.transform = 'scale(1.2)';}} onMouseLeave={(e) => {e.target.style.transform = 'scale(1)';}}><Icon path={mdiDelete} size={1}></Icon></button> 
               </div>
             </div>
 
-            <div>
-              <footer style={{fontSize: '12px', color: 'gray'}}>{today} / {month} / {year} - {hours}:{min}</footer>
+            <div style={{textAlign: 'center', display: 'flex', flexDirection: 'row', margin:'0 auto', flexGrow: 2, flexWrap: 2}}>
+              <input type='date' value={taskDate} style={{textAlign: 'center', width: '110px', border: 'none'}}/>
             </div>
           </div>
       </div>
